80dfc890b39e851a62eed4d084c833f931e4d606
Swoerve <swoerve@gmail.com>
2024-10-15 00:36:16 +0200

    start of native javascript

---
+++ native-javascript/1-Konsol och script element/index.js
@@ -0,0 +1,5 @@
+console.log("Hello World!", "beep boop")
+/* beep
+boop */
+
+// boop beep?
---
+++ native-javascript/3-operatorer aritmetik/index.js
@@ -0,0 +1,34 @@
+//let one = 1
+//let two = '2'
+
+// n + o = 16
+// o - p * 2 = 4
+// o - p = 2
+// p / 4 === 1 är true, alltså är p / 4 = 1 alltså p = 4
+// o - p = 2, 2 + p(4) = 6, o = 6
+/*
+let n = 4
+let o = 12
+let p = 4
+*/
+
+/*
+let before = prompt("mmmm moms:")
+
+alert(before * 0.8)
+*/
+
+/*
+let age = prompt("enter your age: ")
+
+age++
+
+alert("Nästa gång du fyller år blir du " + age)
+*/
+
+let first = prompt("First number:")
+let second = prompt("Second number:")
+
+let res = Math.pow(first, second)
+
+alert(res)
