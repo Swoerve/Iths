6cef13a8c57343f6e915034d3e09fccf67907164
Swoerve <swoerve@gmail.com>
2024-10-17 11:30:53 +0200

    chapter 6

---
+++ native-javascript/6-while-for/index.js
@@ -0,0 +1,20 @@
+//let count = prompt("How many alerts do you want?");
+// let number = 0;
+
+// while (number < count && count) {
+    // alert(number);
+    // number++;
+// }
+
+let count
+
+do {
+    count = prompt()
+} while (!count || isNaN(count))
+
+
+console.log(count)
+  
+for (let number = 0; number < count && count; number++) {
+    alert(number)
+}
