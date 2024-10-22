956fe84d86274919ecc8610afb7e2515743a4171
swoerve <swoerve@gmail.com>
2024-10-22 09:18:05 +0200

    chapter 10 arrays and objects

---
+++ native-javascript/10-array-and-objects/index.js
@@ -0,0 +1,11 @@
+// const a = [{one: 1, two: 2, three: 3},{one: 1, two: 2, three: 3},{one: 1, two: 2, three: 3}]
+
+// const o = {
+//     one: [1,2,3],
+//     two: [1,2,3],
+//     three: [1,2,3],
+// }
+
+// let a = [['a'], ['b', 'c']]
+// let o = {a: [1]}
+let o = {a: [0, 1, {v: [0, {w: {x: 5}}]}]}
