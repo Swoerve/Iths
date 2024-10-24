3c890b7eef383ee3c974278ae852194de6bfe059
Swoerve <swoerve@gmail.com>
2024-10-24 09:50:33 +0200

    chapter 12, returns

---
+++ native-javascript/12-returnvalues/index.js
@@ -0,0 +1,15 @@
+// function greeting(word) {
+//     return "Hello " + word + "!"
+// }
+//
+// function successor(num) {
+//     return num + 1
+// }
+//
+// function difference(a, b) {
+//     return a - b
+// }
+
+function factorial(n) {
+    return n === 1 ? 1 : n * factorial(n - 1)
+}
