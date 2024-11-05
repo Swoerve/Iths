b1af6c2b97129dd7f069c7fde33e4f44928dffc0
Swoerve <swoerve@gmail.com>
2024-11-05 10:05:13 +0100

    chapter 15

---
+++ native-javascript/15-functions2/index.js
@@ -0,0 +1,14 @@
+function greeting() {
+    return 'Hello World!'
+}
+
+// let f = () => {
+//     alert()
+//     return true
+// } 
+
+// let f = () => 0
+
+// let f = i => [i]
+
+let f = (item_one, item_two) => ({ k: item_one, l: item_two })
