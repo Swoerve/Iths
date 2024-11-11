5b471b5c2eecedab5890e156dcf9b79f2da0b6b5
swoerve <swoerve@gmail.com>
2024-11-11 10:14:16 +0100

    chapter 11

---
+++ native-javascript/11-functions-and-params/index.js
@@ -0,0 +1,16 @@
+// let displayDialogBox = function (text) {
+//     alert(text)
+// }
+
+// let concatenateAndLog = function (a, b) {
+//     let c = a + b
+//     console.log(c)
+// }
+
+// let logLength = function (arr) {
+//     console.log(arr.length)
+// }
+
+let lookup = function (ob, st) { //object and string
+    console.log(ob[st])
+}
